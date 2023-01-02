e34bebd2678898d84f75e0550d4ad8de2228c33c
scud <alban.escudier@hotmail.fr>
2023-01-02 16:54:25 +0100

    first commit

---
+++ View/inc/footer.php
@@ -0,0 +1,17 @@
+
+
+
+<footer class="page-footer ">
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row footer-row">
+        <div class="col l6 s12 yellow-text">
+          Copyright © <?= date("Y"); ?> Jean Forteroche
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <a href="<?=ROOT_URL?>blog_legalNotice.html" class="yellow-text">Mentions légales</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
